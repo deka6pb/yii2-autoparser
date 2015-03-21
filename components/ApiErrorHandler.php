5e5b1daa7a046a2a8ab00f29038eea4465d597fd
Mickhail M <deka6pb@rambler.ru>
2015-03-21 10:48:37 +0300

    + apierrorhandler

---
+++ components/ApiErrorHandler.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace deka6pb\autoparser\components;
+
+use Yii;
+
+class ApiErrorHandler extends \yii\web\ErrorHandler
+{
+
+    /**
+     * @inheridoc
+     */
+
+    protected function renderException($exception)
+    {
+
+    }
+}
