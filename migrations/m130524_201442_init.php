81d604cf154076340178d04eaf710980589233e1
Mickhail M <deka6pb@rambler.ru>
2015-03-21 11:35:20 +0300

    fix

--- migrations/m130524_201442_init.php
+++ migrations/m130524_201442_init.php
@@ -13,19 +13,6 @@ class m130524_201442_init extends Migration
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
 
-        $this->createTable('{{%user}}', [
-            'id' => Schema::TYPE_PK,
-            'username' => Schema::TYPE_STRING . ' NOT NULL',
-            'auth_key' => Schema::TYPE_STRING . '(32) NOT NULL',
-            'password_hash' => Schema::TYPE_STRING . ' NOT NULL',
-            'password_reset_token' => Schema::TYPE_STRING,
-            'email' => Schema::TYPE_STRING . ' NOT NULL',
-
-            'status' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT 10',
-            'created_at' => Schema::TYPE_INTEGER . ' NOT NULL',
-            'updated_at' => Schema::TYPE_INTEGER . ' NOT NULL',
-        ], $tableOptions);
-
         $this->createTable('{{%posts}}', [
             'id' => Schema::TYPE_PK,
             'type' => 'tinyint(1) NOT NULL',
@@ -58,8 +45,7 @@ class m130524_201442_init extends Migration
     public function down()
     {
         echo "m130524_201442_init cannot be reverted.\n";
-
-        $this->dropTable('{{%user}}');
+        
         $this->dropTable('{{%posts}}');
         $this->dropTable('{{%files}}');
         $this->dropTable('{{%post_file}}');
